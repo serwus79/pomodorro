bf4dd5866e99a7b02a38d7c0600071cec3f6dc53
Krzysztof Chomicz <krzysztofchomicz@MacBook-Pro-Krzysztof.local>
2018-10-19 15:07:04 +0200

    first storybook

---
+++ src/stories/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import Link from "../components/atoms/link";
+
+// storiesOf("Welcome", module).add("to Storybook", () => (
+//   <Welcome showApp={linkTo("Button")} />
+// ));
+// Przykład:
+// storiesOf("Button", module)
+//   .add("with text", () => (
+//     <Button onClick={action("clicked")}>Hello Button</Button>
+//   ))
+//   .add("with some emoji", () => (
+//     <Button onClick={action("clicked")}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
+storiesOf("Atoms/Link", module).add("with text", () => <Link>Hello Link</Link>);
