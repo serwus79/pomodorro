160e3843b8077ac33fe96d4128ec4ba72d3e2f5e
Krzysztof Chomicz <krzysztofchomicz@macbook-pro-krzysztof.home>
2018-10-22 14:59:17 +0200

    Fixed basic tests.

--- src/components/atoms/progress-bar/progress-bar.test.js
+++ src/components/atoms/progress-bar/progress-bar.test.js
@@ -4,6 +4,6 @@ import ProgressBar from "./progress-bar";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<ProgressBar />, div);
+  ReactDOM.render(<ProgressBar progress={10} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/molecules/timer-display/timer-display.test.js
+++ src/components/molecules/timer-display/timer-display.test.js
@@ -4,6 +4,6 @@ import TimerDisplay from "./timer-display";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<TimerDisplay />, div);
+  ReactDOM.render(<TimerDisplay timeTotal={10} timeRemaining={3} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
